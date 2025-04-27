db2724d6df1562cccdc34ced7cfd0da9f1e81d1d
ThierryBab <Thierry.babault.20@hotmail.fr>
2025-04-27 12:30:52 -0400

    formatage

--- multiplication_matrice.cpp
+++ multiplication_matrice.cpp
@@ -1,19 +1,23 @@
 #include <emscripten.h>
 
-extern "C" {
+extern "C"
+{
 
-// Multiplie deux matrices carrées de taille n x n
-EMSCRIPTEN_KEEPALIVE
-void multiplyMatrices(int* A, int* B, int* C, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            int sum = 0;
-            for (int k = 0; k < n; k++) {
-                sum += A[i * n + k] * B[k * n + j];
+    // Multiplie deux matrices carrées de taille n x n
+    EMSCRIPTEN_KEEPALIVE
+    void multiplyMatrices(int *A, int *B, int *C, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int sum = 0;
+                for (int k = 0; k < n; k++)
+                {
+                    sum += A[i * n + k] * B[k * n + j];
+                }
+                C[i * n + j] = sum;
             }
-            C[i * n + j] = sum;
         }
     }
-}
-
 }
